546444753b480cc6d592c4ac5e85ef2b0e7cda65
EmreKaratas64 <emrekaratas076@gmail.com>
2024-08-02 20:17:28 +0300

    Created Order microservice in Onion architecture

---
+++ WebCommerce/Services/Order/Core/WebCommerce.Order.Application/Features/CQRS/Queries/AddressQueries/GetAddressByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace WebCommerce.Order.Application.Features.CQRS.Queries.AddressQueries
+{
+    public class GetAddressByIdQuery
+    {
+        public int Id { get; set; }
+
+        public GetAddressByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ WebCommerce/Services/Order/Core/WebCommerce.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace WebCommerce.Order.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task<T> CreateAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<T> DeleteAsync(int id);
+
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+    }
+}
